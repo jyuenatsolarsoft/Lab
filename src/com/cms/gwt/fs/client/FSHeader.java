77955051d3e4911eb92a762e36c922aea889c7a5
Joseph Yuen <joseph.yuen@solarsoft.com>
2012-10-04 10:02:36 -0400

    test merge from 1.1 to unchanged master

--- src/com/cms/gwt/fs/client/FSHeader.java
+++ src/com/cms/gwt/fs/client/FSHeader.java
@@ -17,9 +17,9 @@ import com.google.gwt.user.client.ui.Widget;
 public class FSHeader extends Composite {
 
 	/** The About dialog of the application. */ abc
-	private static DialogBox aboutDialog;
+	private static DialogBox aboutDialog; 
 
-	/** The main panel of the header. */
+	/** The main panel of the header. */ abc
 	private HorizontalPanel outer;
 
 	/** Contains the user information. */
